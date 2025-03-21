0efdfed73630b9522f2c38c9ce895e8a1aa4523f
5UN913 <sxh061019@163.com>
2025-03-21 20:16:14 +0800

    EP30-1.c

--- EP30-1.c
+++ EP30-1.c
@@ -5,5 +5,28 @@
 	> Created Time: Fri 21 Mar 2025 06:59:25 PM CST
  ************************************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
+
+int calculator(int n);
+
+int main() {
+    int n, ans = 0;
+    for (int i = 2; i <= 2177280; i++) {
+        if (i == calculator(i)) ans += i;
+    }
+    printf("%d\n", ans);
+    return 0;
+}
+
+int calculator(int n) {
+    int sum = 0;
+    int original = n; // 保存原始n的值，避免修改循环变量导致的问题
+    while (original != 0) {
+        int digit = original % 10;
+        sum += digit * digit * digit * digit * digit;
+        original /= 10;
+    }
+    return sum;
+}
